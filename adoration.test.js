46ac348103f58a8e8eacf4c877ad6cc13d92ec23
Ryan Ung <ung.ryan1@gmail.com>
2022-09-15 10:36:25 +0100

    Add more testing

--- adoration.test.js
+++ adoration.test.js
@@ -1,3 +1,5 @@
+const each = require('jest-each').default;
+
 const {showHowMuchILoveYou} = require('./adoration')
 
 describe("showHowMuchILoveYou", () => {
@@ -10,9 +12,11 @@ describe("showHowMuchILoveYou", () => {
         expect(showHowMuchILoveYou instanceof Function).toEqual(true);
     })
 
-    it("Returns a string with the correct number of 'i's", () => {
-        expect(showHowMuchILoveYou(5)).toBe("I love you thiiiiis much!")
-    })
+    each([["I love you thiiiiis much!", 5], 
+            ["I love you thiiis much!", 3]
+            ]).test("Returns %s when passd %s ", (expected, amount) => {
+                expect(showHowMuchILoveYou(amount)).toBe(expected);
+            })
 
     it("Throws an error when passed a string", () => {
         expect(() => {
